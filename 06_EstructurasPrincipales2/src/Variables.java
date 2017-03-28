f67285cbb03e4af21e2819573701d542288afb06
Vassilly <smunozs1989@gmail.com>
2017-03-27 22:55:13 -0300

    Add files via upload

---
+++ 06_EstructurasPrincipales2/src/Variables.java
@@ -0,0 +1,17 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		byte edad = 35; //-128,128 ->  declaracion e inicializacion en una linea
+		
+		int edad_2 = 50;
+		System.out.println(edad);
+		System.out.println(edad_2);
+		
+		/*
+		 * Muchos comentarios
+		 * por Sergio.
+		 * */
+	}
+
+}
